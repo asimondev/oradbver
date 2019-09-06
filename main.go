f1fd031343af8ca4f9094fe20a459a723403fe8d
Andrej Simon <oracle@hasi_ol7>
2019-09-06 22:43:33 +0200

    Refactored Database type.

--- main.go
+++ main.go
@@ -12,23 +12,76 @@ import (
 	"strings"
 )
 
+type Database struct {
+	User string
+	Password string
+	Role string
+	Connect string
+}
+
+func NewDatabase(u, p, r, c *string) Database {
+	return 	Database{
+		User: *u, Password: *p, Role: *r, Connect: *c,
+	}
+}
+
+func (d *Database) ReadArgs() {
+	d.Role = strings.ToLower(d.Role)
+
+	if d.Role != "sysdba" && d.Role != "sysbackup" && d.Role != "" {
+		log.Fatal("Error: unknown system privilege " + d.Role)
+	}
+
+	if d.User != "" {
+		if d.Password == "" {
+			log.Fatal("Error: password is missing.")
+		}
+	} else {
+		if d.Password != "" {
+			log.Fatal("Error: password specified for unknown user.")
+		}
+
+		if d.Role == "" {
+			d.Role = "sysdba"
+		}
+	}
+}
+
+func (d Database) String() string {
+	return fmt.Sprintf("Database => User: %s Password: %s Role: %s Connect: %s\n",
+		d.User, d.Password, d.Role, d.Connect)
+}
+
+func (d *Database) ConnectString() string {
+	var login string
+	if d.User != "" {
+		login = d.User + ":" + d.Password
+	}
+
+	var role string
+	if d.Role == "sysdba" || d.User == "" && d.Role == "sysdba" {
+		role = "sysdba=1& "
+	}
+	sessions := 1
+
+	return fmt.Sprintf("oracle://%s?%spoolMinSessions=%d& " +
+		"poolMaxSessions=%d& poolInrement=0", login, role, sessions, sessions)
+}
+
 func main() {
 	var u = flag.String("u", "", "database user")
 	var p = flag.String("p", "", "user password")
 	var r = flag.String("r", "", "system privilege")
+	var c = flag.String("d", "", "connect string")
 
 	flag.Parse()
 
-	usr, pwd, role := checkArgs(*u, *p, *r)
-	fmt.Printf("User: %s Password: %s Role: %s\n", usr, pwd, role)
+	d := NewDatabase(u, p, r, c)
+	d.ReadArgs()
+	fmt.Println(d)
 
-	if role != "" {
-		role = "sysdba=1&"
-	}
-	sessions := 1
-
-	con := fmt.Sprintf("oracle://?%spoolMinSessions=%d& poolMaxSessions=%d& poolInrement=0",
-		role, sessions, sessions)
+	con := d.ConnectString()
+	fmt.Println(con)
 	db, err := sql.Open("goracle", con)
 	if err != nil {
 		fmt.Printf("Error: database connect error %v (%s).", err, con)
@@ -40,29 +93,29 @@ func main() {
 	writeJSON(release, ver, checkRAC(db), checkCDB(db, ver))
 }
 
-func checkArgs(usr, pwd, role string) (string, string, string) {
-	role = strings.ToLower(role)
-
-	if role != "sysdba" && role != "" {
-		log.Fatal("Error: unknown system privilege " + role)
-	}
-
-	if usr != "" {
-		if pwd == "" {
-			log.Fatal("Error: password is missing.")
-		}
-	} else {
-		if pwd != "" {
-			log.Fatal("Error: password specified for unknown user.")
-		}
-
-		if role == "" {
-			role = "sysdba"
-		}
-	}
-
-	return usr, pwd, role
-}
+//func checkArgs(usr, pwd, role string) (string, string, string) {
+//	role = strings.ToLower(role)
+//
+//	if role != "sysdba" && role != "" {
+//		log.Fatal("Error: unknown system privilege " + role)
+//	}
+//
+//	if usr != "" {
+//		if pwd == "" {
+//			log.Fatal("Error: password is missing.")
+//		}
+//	} else {
+//		if pwd != "" {
+//			log.Fatal("Error: password specified for unknown user.")
+//		}
+//
+//		if role == "" {
+//			role = "sysdba"
+//		}
+//	}
+//
+//	return usr, pwd, role
+//}
 
 func checkVersion(db *sql.DB) (string, int) {
 	ver, err := goracle.ServerVersion(db)

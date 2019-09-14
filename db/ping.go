4fd72771f6ed43dce4ea4548aedabe55ffbf3e99
Andrej Simon <oracle@hasi_ol7>
2019-09-14 12:31:56 +0200

    Refactored getSessionDetails().

--- db/ping.go
+++ db/ping.go
@@ -1,23 +1,43 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"os"
 	"time"
 )
 
-type DbDetails struct {
-	Role string
-	UniqueName string
-	InstanceName string
-	Server string
-	Service string
+func StartPinging(cn *Connect) {
+	abort := make(chan struct{})
+	fmt.Println("\nPress Return to stop the pings...")
+	go func(){
+		os.Stdin.Read(make([]byte, 1)) // read one byte from input
+		abort <- struct{}{}
+	}()
+
+	ticker := time.NewTicker(1 * time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			PingDatabase(cn)
+			fmt.Println()
+		case <-abort:
+			ticker.Stop()
+			return
+		}
+	}
 }
 
-func (d DbDetails) String() string {
-	return fmt.Sprintf("Inst: %s  Host: %s  Service: %s  Db.Name: %s  Db.Role: %s",
-		d.InstanceName, d.Server, d.Service, d.UniqueName, d.Role)
+func PingOnce(cn *Connect) int{
+	err := PingDatabase(cn);
+	fmt.Println()
+	if err != nil {
+		return 1
+	}
+
+	return 0
 }
 
 func PingDatabase(cn *Connect) error {
@@ -37,24 +57,34 @@ func PingDatabase(cn *Connect) error {
 	return getSessionDetails(db)
 }
 
+type DbDetails struct {
+	Role string
+	UniqueName string
+	InstanceName string
+	Server string
+	Service string
+}
+
+func (d DbDetails) String() string {
+	return fmt.Sprintf("Inst: %s  Host: %s  Service: %s  Db.Name: %s  Db.Role: %s",
+		d.InstanceName, d.Server, d.Service, d.UniqueName, d.Role)
+}
 
 func getSessionDetails(db *sql.DB) error {
+	var d DbDetails
+
+	ctx := context.Background()
 	stmt := `select sys_context('userenv', 'db_unique_name'),
 		sys_context('userenv', 'instance_name'),
 		sys_context('userenv', 'server_host'),
 		sys_context('userenv', 'service_name')
 		from dual`
 
-	rows, err := db.Query(stmt)
+
+	err := db.QueryRowContext(ctx, stmt).Scan(&d.UniqueName, &d.InstanceName, &d.Server, &d.Service)
 	if err != nil {
 		fmt.Printf("database query error %v", err)
 		return err
-	}
-	defer rows.Close()
-
-	var d DbDetails
-	for rows.Next() {
-		rows.Scan(&d.UniqueName, &d.InstanceName, &d.Server, &d.Service)
 	}
 
 	d.Role, err = getRole(db)
@@ -67,34 +97,3 @@ func getSessionDetails(db *sql.DB) error {
 	return nil
 }
 
-func StartPinging(cn *Connect) {
-	abort := make(chan struct{})
-	fmt.Println("\nPress Return to stop the pings...")
-	go func(){
-		os.Stdin.Read(make([]byte, 1)) // read one byte from input
-		abort <- struct{}{}
-	}()
-
-	ticker := time.NewTicker(1 * time.Second)
-
-	for {
-		select {
-			case <-ticker.C:
-				PingDatabase(cn)
-				fmt.Println()
-		case <-abort:
-			ticker.Stop()
-			return
-		}
-	}
-}
-
-func PingOnce(cn *Connect) int{
-	err := PingDatabase(cn);
-	fmt.Println()
-	if err != nil {
-		return 1
-	}
-
-	return 0
-}
